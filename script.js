a6e88f8ddc78d1a6b88e7120859db7f8a6d2e7a8
laurynvar <laurynas.varkauskas@gmail.com>
2023-02-18 14:17:05 +0200

    Constructor function for sum method added

---
+++ script.js
@@ -0,0 +1,16 @@
+/* ------------------------------ TASK 8 --------------------------------------------
+Sukurkite konstruktoriaus funkciją "Calculator" (naudokite ES5), kuri sukuria objektus su 3 metodais:
+sum() - priima du skaičius ir grąžina jų sumą.
+subtraction() - priima du skaičius ir grąžina jų skirtumą.
+multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
+division() - priima du skaičius ir grąžina jų dalybos rezultatą;
+------------------------------------------------------------------------------------ */
+
+function calculator() {
+  this.sum = function (a, b) {
+    return a + b;
+  };
+}
+
+const calculate = new calculator();
+console.log(calculate.sum(10, 20));
